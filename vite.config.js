4b6c23f313ccfb8c4cef941cf478846e41f27cf2
vanbui <vanbt@zigvy.com>
2023-10-04 16:35:27 +0700

    excude example folder from tsc command

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig ({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "my-library",
+      name: "react-sipjs-client",
       fileName: "index",
     },
     rollupOptions: {
